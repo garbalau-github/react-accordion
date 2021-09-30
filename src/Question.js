deeef57e34696de7bd7a058b7f8912ff00fb6e1c
Nick Garbalau <garbalaunick@protonmail.com>
2021-09-30 21:15:39 +0300

    Simple accordion project

---
+++ src/Question.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Question = ({ title, info }) => {
+
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{ title }</h4>
+        <button onClick={ () => setShowInfo(!showInfo) } className='btn'>
+          {
+            showInfo ? <span>-</span> : <span>+</span>
+          }
+        </button>
+      </header>
+      {
+        showInfo && <p>{ info }</p>
+      }
+    </article>
+  )
+};
+
+export default Question;
